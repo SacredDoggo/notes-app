5f781d1a536ea282412e80dbe002a1e6c290b1e3
SacredDoggo <omvkrishna0852@gmail.com>
2024-02-09 13:43:06 +0530

    readme and file upload changes

--- components/editor.tsx
+++ components/editor.tsx
@@ -40,7 +40,7 @@ const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
-    uploadFile: handleUpload
+    uploadFile: uploadToTmpFilesDotOrg_DEV_ONLY
   });
 
   return (
