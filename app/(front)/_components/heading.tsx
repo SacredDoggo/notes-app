4c4d2529b7712dff7cfbcf85c0ee718c5ee74d88
SacredDoggo <omvkrishna0852@gmail.com>
2024-02-09 14:20:51 +0530

    fix error: react/no-unescaped-entities

--- app/(front)/_components/heading.tsx
+++ app/(front)/_components/heading.tsx
@@ -14,10 +14,10 @@ export const Heading = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-3xl space-y-8">
       <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl">
-        Welcome to OVK's notes app. Note taking made easy by <span className="underline">Fable</span>.
+        {`Welcome to OVK's notes app. Note taking made easy by`} <span className="underline">Fable</span>.
       </h1>
       <h3 className="text-base font-medium pt-2 sm:text-xl md:text-2xl">
-        It's a online workspace for your important snippets of data.
+        {`It's a online workspace for your important snippets of data.`}
       </h3>
       {isLoading && (
         <Spinner size="lg" />
