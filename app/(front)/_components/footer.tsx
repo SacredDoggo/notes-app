2046cfaa73e656b45ed6ef0b523c458887fc2680
SacredDoggo <omvkrishna0852@gmail.com>
2024-02-09 14:21:44 +0530

    z index fix

--- app/(front)/_components/footer.tsx
+++ app/(front)/_components/footer.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
+    <div className="flex items-center w-full p-6 bg-background dark:bg-[#1F1F1F]">
       <div className="dark:bg-[#1F1F1F] flex w-full items-center justify-between md:justify-end gap-x-2 text-muted-foreground">
         <Button variant="ghost" size="sm">
           Privacy Policy
